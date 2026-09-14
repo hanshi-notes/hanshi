94fdba99be0f69fab8b8336f56765d78616e40dd
Gerard Porto <gporto@gmail.com>
2026-09-14 09:41:14 +0200

    Set app version to 0.3.5

--- Scripts/package_app.sh
+++ Scripts/package_app.sh
@@ -35,7 +35,7 @@ cat > "$APP/Contents/Info.plist" <<'PLIST'
     <key>CFBundleIdentifier</key><string>com.hanshi.app</string>
     <key>CFBundleExecutable</key><string>Hanshi</string>
     <key>CFBundlePackageType</key><string>APPL</string>
-    <key>CFBundleShortVersionString</key><string>0.1.0</string>
+    <key>CFBundleShortVersionString</key><string>0.3.5</string>
     <key>CFBundleVersion</key><string>1</string>
     <key>LSMinimumSystemVersion</key><string>15.0</string>
     <key>NSHighResolutionCapable</key><true/>
